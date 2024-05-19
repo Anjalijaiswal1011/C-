178d588f65983da2d8932d688d2daa6d3b0225f1
Anjalijaiswal1011 <anjalijayswal54@gmail.com>
2024-05-19 20:34:45 +0530

    Codes

---
+++ 20240519-202740.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+int a = 1;
+{
+
+while(a) { // the while condition will always be mer as will always return true.}
+
+cout << "Infinite loop\n" << endl;
+
+}
